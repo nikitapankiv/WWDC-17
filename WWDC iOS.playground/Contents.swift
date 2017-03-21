6b6fab2734b1d41a160676b85ec3bbceeb78cfcf
Nikita Pankiv <nikita.pankiv@techmagic.co>
2017-03-21 16:02:35 +0200

    creating view for animation

--- WWDC iOS.playground/Contents.swift
+++ WWDC iOS.playground/Contents.swift
@@ -5,7 +5,9 @@ import UIKit
 import PlaygroundSupport
 
 let sineView = SineWaveView(frame: CGRect(x: 0, y: 0, width: 500, height: 200))
-sineView.backgroundColor = .white
+
+
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 PlaygroundPage.current.liveView = sineView
+
